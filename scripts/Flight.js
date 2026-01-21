029f31d5bcb03677c47bee087b60741d4f781528
floris <104316@glr.nl>
2026-01-21 09:01:13 +0100

    camelCase

--- scripts/Flight.js
+++ scripts/Flight.js
@@ -6,33 +6,30 @@ const spaceTable  = {
 
 let distanceElement = document.getElementById('distance')
 let speedElement = document.getElementById('speed')
-let estimatedTimeElement = document.getElementById('estimatedTime')
+let estimatedTimeElement = document.getElementById('estimated-time')
 
 function updateFlightInfo() {
     const Time = new Date().getTime()
-    const timesince = Time - spaceTable.left
+    const timeSince = Time - spaceTable.left
 
-    let secondsSince = timesince / 1000
+    let secondsSince = timeSince / 1000
     let distanceTraveled = spaceTable.speed * secondsSince
-
-    console.log(timesince, distanceTraveled)
 
     distanceElement.textContent = (distanceTraveled / 1000000000).toFixed(1) + ' M KM / 54.6 M KM'
     speedElement.textContent = spaceTable.speed.toLocaleString() + ' M/S'
 
-    const distanceleft = spaceTable.distance - distanceTraveled
-    const secondsleft = distanceleft / spaceTable.speed
+    const distanceLeft = spaceTable.distance - distanceTraveled
+    const secondsLeft = distanceLeft / spaceTable.speed
 
-    let smallchange = (Math.random() - 0.5) * 0.02 * spaceTable.speed
-    let speedtext = Math.round(spaceTable.speed + smallchange)
-    speedElement.textContent = speedtext.toLocaleString() + ' M/S'
+    let smallChange = (Math.random() - 0.5) * 0.02 * spaceTable.speed
+    let speedText = Math.round(spaceTable.speed + smallChange)
+    speedElement.textContent = speedText.toLocaleString() + ' M/S'
 
-    let days = Math.floor(secondsleft / 86400)
-    let hours = Math.floor((secondsleft % 86400) / 3600)
-    let minutes = Math.floor((secondsleft % 3600) / 60)
-    let seconds = Math.floor(secondsleft % 60)
+    let days = Math.floor(secondsLeft / 86400)
+    let hours = Math.floor((secondsLeft % 86400) / 3600)
+    let minutes = Math.floor((secondsLeft % 3600) / 60)
+    let seconds = Math.floor(secondsLeft % 60)
 
-    console.log(distanceleft, secondsleft)
     estimatedTimeElement.textContent = `${days}D ${hours}H ${minutes}M ${seconds}S`
 }
 
--- scripts/Movie.js
+++ scripts/Movie.js
@@ -1,114 +1,114 @@
 let containerDiv = document.getElementById("container")
 let screenDiv = document.getElementById("screen")
-let videoDiv = document.getElementById("Trailer")
+let videoDiv = document.getElementById("trailer")
 let infoDiv = document.getElementById("information")
-let movieImage = document.getElementById("movieImage")
+let movieImage = document.getElementById("movie-image")
 
 const movies = {
-    'space odyssey': "./Videos/2001 A SPACE ODYSSEY - Trailer - Warner Bros. (720p, h264).mp4",
-    'Interstellar': "./Videos/Interstellar.mp4",
+    'space odyssey': "../Videos/2001 A SPACE ODYSSEY - Trailer - Warner Bros. (720p, h264).mp4",
+    'Interstellar': "../Videos/Interstellar.mp4",
 }
 
 const movieInfo = {
     'space odyssey': {
-        Title: '2001: A Space Odyssey',
-        IMDB: 8.0,
-        Genre: 'Science Fiction, Mystery, Adventure',
-        Duration: '149 min',
-        Release: '1968',
-        Actors: 'Keir Dullea Gary Lockwood William Sylvester Douglas Rain Daniel Richter',
-        Overview: "Humanity finds a mysterious object buried beneath the lunar surface and sets off to find its origins with the help of HAL 9000, the world's most advanced super computer.",
-        image: "./Images/Movies/SpaceOdyssey.jpg"
+        title: '2001: A Space Odyssey',
+        imdb: 8.0,
+        genre: 'Science Fiction, Mystery, Adventure',
+        duration: '149 min',
+        release: '1968',
+        actors: 'Keir Dullea Gary Lockwood William Sylvester Douglas Rain Daniel Richter',
+        overview: "Humanity finds a mysterious object buried beneath the lunar surface and sets off to find its origins with the help of HAL 9000, the world's most advanced super computer.",
+        image: "../Images/Movies/SpaceOdyssey.jpg"
     },
     'Interstellar': {
-        Title: 'Interstellar',
-        IMDB: 8.5,
-        Genre: 'Adventure Drama Science Fiction',
-        Duration: '169 min',
-        Release: '2014',
-        Actors: 'Matthew McConaughey Anne Hathaway Michael Caine Jessica Chastain Casey Affleck',
-        Overview: "The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
-        image: "./Images/Movies/Interstellar.jpg"
+        title: 'Interstellar',
+        imdb: 8.5,
+        genre: 'Adventure Drama Science Fiction',
+        duration: '169 min',
+        release: '2014',
+        actors: 'Matthew McConaughey Anne Hathaway Michael Caine Jessica Chastain Casey Affleck',
+        overview: "The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
+        image: "../Images/Movies/Interstellar.jpg"
     },
     'gravity': {
-        Title: 'Gravity',
-        IMDB: 7.7,
-        Genre: 'Science Fiction, Thriller',
-        Duration: '91 min',
-        Release: '2013',
-        Actors: 'Sandra Bullock George Clooney Ed Harris',
-        Overview: "Two astronauts work together to survive after an accident leaves them stranded in space.",
-        image: "./Images/Movies/Gravity.jpg"
+        title: 'Gravity',
+        imdb: 7.7,
+        genre: 'Science Fiction, Thriller',
+        duration: '91 min',
+        release: '2013',
+        actors: 'Sandra Bullock George Clooney Ed Harris',
+        overview: "Two astronauts work together to survive after an accident leaves them stranded in space.",
+        image: "../Images/Movies/Gravity.jpg"
     },
     'the martian': {
-        Title: 'The Martian',
-        IMDB: 8.0,
-        Genre: 'Science Fiction, Adventure, Drama',
-        Duration: '144 min',
-        Release: '2015',
-        Actors: 'Matt Damon Jessica Chastain Kristen Wiig Jeff Daniels',
-        Overview: "An astronaut becomes stranded on Mars and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
-        image: "./Images/Movies/Martian.jpg"
+        title: 'The Martian',
+        imdb: 8.0,
+        genre: 'Science Fiction, Adventure, Drama',
+        duration: '144 min',
+        release: '2015',
+        actors: 'Matt Damon Jessica Chastain Kristen Wiig Jeff Daniels',
+        overview: "An astronaut becomes stranded on Mars and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
+        image: "../Images/Movies/Martian.jpg"
     },
     'apollo 13': {
-        Title: 'Apollo 13',
-        IMDB: 7.6,
-        Genre: 'Drama, History',
-        Duration: '140 min',
-        Release: '1995',
-        Actors: 'Tom Hanks Bill Paxton Kevin Bacon Gary Sinise Ed Harris',
-        Overview: "NASA must devise a strategy to return Apollo 13 to Earth safely after the spacecraft undergoes massive internal damage.",
-        image: "./Images/Movies/Apollo13.jpg"
+        title: 'Apollo 13',
+        imdb: 7.6,
+        genre: 'Drama, History',
+        duration: '140 min',
+        release: '1995',
+        actors: 'Tom Hanks Bill Paxton Kevin Bacon Gary Sinise Ed Harris',
+        overview: "NASA must devise a strategy to return Apollo 13 to Earth safely after the spacecraft undergoes massive internal damage.",
+        image: "../Images/Movies/Apollo13.jpg"
     },
     'star wars': {
-        Title: 'Star Wars: A New Hope',
-        IMDB: 8.6,
-        Genre: 'Action, Adventure, Fantasy',
-        Duration: '121 min',
-        Release: '1977',
-        Actors: 'Mark Hamill Harrison Ford Carrie Fisher Alec Guinness',
-        Overview: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy.",
-        image: "./Images/Movies/StarWars.jpg"
+        title: 'Star Wars: A New Hope',
+        imdb: 8.6,
+        genre: 'Action, Adventure, Fantasy',
+        duration: '121 min',
+        release: '1977',
+        actors: 'Mark Hamill Harrison Ford Carrie Fisher Alec Guinness',
+        overview: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy.",
+        image: "../Images/Movies/StarWars.jpg"
     },
     'arrival': {
-        Title: 'Arrival',
-        IMDB: 7.9,
-        Genre: 'Drama, Science Fiction',
-        Duration: '116 min',
-        Release: '2016',
-        Actors: 'Amy Adams Jeremy Renner Forest Whitaker',
-        Overview: "A linguist works with the military to communicate with alien lifeforms after twelve mysterious spacecraft appear around the world.",
-        image: "./Images/Movies/Arrival.jpg"
+        title: 'Arrival',
+        imdb: 7.9,
+        genre: 'Drama, Science Fiction',
+        duration: '116 min',
+        release: '2016',
+        actors: 'Amy Adams Jeremy Renner Forest Whitaker',
+        overview: "A linguist works with the military to communicate with alien lifeforms after twelve mysterious spacecraft appear around the world.",
+        image: "../Images/Movies/Arrival.jpg"
     },
     'ad astra': {
-        Title: 'Ad Astra',
-        IMDB: 6.5,
-        Genre: 'Adventure, Drama, Mystery',
-        Duration: '123 min',
-        Release: '2019',
-        Actors: 'Brad Pitt Tommy Lee Jones Ruth Negga',
-        Overview: "Astronaut Roy McBride undertakes a mission across an unforgiving solar system to uncover the truth about his missing father.",
-        image: "./Images/Movies/Astra.jpg"
+        title: 'Ad Astra',
+        imdb: 6.5,
+        genre: 'Adventure, Drama, Mystery',
+        duration: '123 min',
+        release: '2019',
+        actors: 'Brad Pitt Tommy Lee Jones Ruth Negga',
+        overview: "Astronaut Roy McBride undertakes a mission across an unforgiving solar system to uncover the truth about his missing father.",
+        image: "../Images/Movies/Astra.jpg"
     },
     'first man': {
-        Title: 'First Man',
-        IMDB: 7.3,
-        Genre: 'Biography, Drama, History',
-        Duration: '141 min',
-        Release: '2018',
-        Actors: 'Ryan Gosling Claire Foy Jason Clarke',
-        Overview: "A look at the life of the astronaut, Neil Armstrong, and the legendary space mission that led him to become the first man to walk on the Moon.",
-        image: "./Images/Movies/FirstMan.png"
+        title: 'First Man',
+        imdb: 7.3,
+        genre: 'Biography, Drama, History',
+        duration: '141 min',
+        release: '2018',
+        actors: 'Ryan Gosling Claire Foy Jason Clarke',
+        overview: "A look at the life of the astronaut, Neil Armstrong, and the legendary space mission that led him to become the first man to walk on the Moon.",
+        image: "../Images/Movies/FirstMan.png"
     },
     'hidden figures': {
-        Title: 'Hidden Figures',
-        IMDB: 7.8,
-        Genre: 'Biography, Drama, History',
-        Duration: '127 min',
-        Release: '2016',
-        Actors: 'Taraji P. Henson Octavia Spencer Janelle Monáe',
-        Overview: "The story of a team of female African-American mathematicians who served a vital role in NASA during the early years of the U.S. space program.",
-        image: "./Images/Movies/Hidden.jpg"
+        title: 'Hidden Figures',
+        imdb: 7.8,
+        genre: 'Biography, Drama, History',
+        duration: '127 min',
+        release: '2016',
+        actors: 'Taraji P. Henson Octavia Spencer Janelle Monáe',
+        overview: "The story of a team of female African-American mathematicians who served a vital role in NASA during the early years of the U.S. space program.",
+        image: "../Images/Movies/Hidden.jpg"
     },
 }
 
@@ -135,31 +135,28 @@ for (const movie in movieInfo) {
     containerDiv.appendChild(newDiv)
 }
 
-function clickedMovie(movie) {
-    let title = document.getElementById("Title")
-    let imdb = document.getElementById("IMDB")
-    let genre = document.getElementById("Genre")
-    let duration = document.getElementById("Duration")
-    let release = document.getElementById("Release")
-    let actors = document.getElementById("Actors")
-    let overview = document.getElementById("Overview")
+let title = document.getElementById("title")
+let imdb = document.getElementById("imdb")
+let genre = document.getElementById("genre")
+let duration = document.getElementById("duration")
+let release = document.getElementById("release")
+let actors = document.getElementById("actors")
+let overview = document.getElementById("overview")
 
-    console.log(movie)
+function clickedMovie(movie) {
     document.body.style.cursor = "progress"
 
     if (movieInfo[movie]) {
-        videoDiv.src = movies[movie] || "./Videos/placeholder.mp4"
-        title.innerHTML = movieInfo[movie].Title
-        overview.innerHTML = movieInfo[movie].Overview
-        imdb.innerHTML = movieInfo[movie].IMDB
-        genre.innerHTML = movieInfo[movie].Genre
-        duration.innerHTML = movieInfo[movie].Duration
-        release.innerHTML = movieInfo[movie].Release
-        actors.innerHTML = movieInfo[movie].Actors
+        videoDiv.src = movies[movie] || "../Videos/placeholder.mp4"
+        title.innerHTML = movieInfo[movie].title
+        overview.innerHTML = movieInfo[movie].overview
+        imdb.innerHTML = movieInfo[movie].imdb
+        genre.innerHTML = movieInfo[movie].genre
+        duration.innerHTML = movieInfo[movie].duration
+        release.innerHTML = movieInfo[movie].release
+        actors.innerHTML = movieInfo[movie].actors
         movieImage.src = movieInfo[movie].image
-        console.log(movieInfo[v])
     } else {
-        console.log(`Nothing found for '${movie}'`)
         document.body.style.cursor = ""
         return
     }
