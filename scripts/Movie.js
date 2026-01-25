6559084bc61bd06fd40879e945cfc482d7921c22
floris <104316@glr.nl>
2026-01-25 11:52:14 +0100

    Textcontent gebrukt in plaats van innerhtml omdat het blijkbaar beter is en beter placeholders gebruikt en alt text voor de photos

--- scripts/Movie.js
+++ scripts/Movie.js
@@ -5,7 +5,7 @@ let movieImage = document.getElementById("movie-image")
 
 const movies = {
     'space odyssey': "http://78.47.119.106:8000/Movies/2001%20A%20Space%20Odyssey%20%281968%29.mp4",
-    'Interstellar': "http://78.47.119.106:8000/Movies/Interstellar%20%282014%29.mp4",
+    'interstellar': "http://78.47.119.106:8000/Movies/Interstellar%20%282014%29.mp4",
     'Moonfall': "http://78.47.119.106:8000/Movies/Moonfall%20%282022%29.mp4",
     'first man': "http://78.47.119.106:8000/Movies/First%20Man%20%282018%29.mp4",
     'avatar': "http://78.47.119.106:8000/Movies/Avatar%20%282009%29.mp4",
@@ -23,13 +23,13 @@ const movies = {
 }
 
 const movieInfo = {
-    'Interstellar': {
+    'interstellar': {
         title: 'Interstellar',
         imdb: 8.5,
         genre: 'Adventure Drama Science Fiction',
         duration: '169 min',
         release: '2014',
-        actors: 'Matthew McConaughey Anne Hathaway Michael Caine Jessica Chastain Casey Affleck',
+        actors: 'Matthew McConaughey, Anne Hathaway, Michael Caine, Jessica Chastain, Casey Affleck',
         overview: "The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.",
         image: "../Images/Movies/Interstellar.jpg"
     },
@@ -39,7 +39,7 @@ const movieInfo = {
         genre: 'Action, Adventure, Science Fiction',
         duration: '130 min',
         release: '2022',
-        actors: 'Halle Berry Patrick Wilson John Bradley',
+        actors: 'Halle Berry, Patrick Wilson, John Bradley',
         overview: "A mysterious force knocks the Moon from its orbit around Earth and sends it hurtling on a collision course with life as we know it. As panic spreads and the world prepares for impact, a ragtag team ventures into space to uncover the real threat behind the disaster and save humanity.",
         image: "../Images/Movies/Moonfall.webp"
     },
@@ -49,7 +49,7 @@ const movieInfo = {
         genre: 'Science Fiction, Mystery, Adventure',
         duration: '149 min',
         release: '1968',
-        actors: 'Keir Dullea Gary Lockwood William Sylvester Douglas Rain Daniel Richter',
+        actors: 'Keir Dullea, Gary Lockwood, William Sylvester, Douglas Rain, Daniel Richter',
         overview: "Humanity finds a mysterious object buried beneath the lunar surface and sets off to find its origins with the help of HAL 9000, the world's most advanced super computer.",
         image: "../Images/Movies/SpaceOdyssey.jpg"
     },
@@ -59,7 +59,7 @@ const movieInfo = {
         genre: 'Science Fiction, Thriller',
         duration: '91 min',
         release: '2013',
-        actors: 'Sandra Bullock George Clooney Ed Harris',
+        actors: 'Sandra Bullock, George Clooney, Ed Harris',
         overview: "Two astronauts work together to survive after an accident leaves them stranded in space.",
         image: "../Images/Movies/Gravity.jpg"
     },
@@ -69,7 +69,7 @@ const movieInfo = {
         genre: 'Science Fiction, Adventure, Drama',
         duration: '144 min',
         release: '2015',
-        actors: 'Matt Damon Jessica Chastain Kristen Wiig Jeff Daniels',
+        actors: 'Matt Damon, Jessica Chastain, Kristen Wiig, Jeff Daniels',
         overview: "An astronaut becomes stranded on Mars and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
         image: "../Images/Movies/Martian.jpg"
     },
@@ -79,7 +79,7 @@ const movieInfo = {
         genre: 'Action, Adventure, Science Fiction',
         duration: '155 min',
         release: '2021',
-        actors: 'Timothée Chalamet Rebecca Ferguson Oscar Isaac Josh Brolin Zendaya',
+        actors: 'Timothée Chalamet, Rebecca Ferguson, Oscar Isaac, Josh Brolin, Zendaya',
         overview: "Paul Atreides, a gifted young man born into a great destiny, travels to the desert planet Arrakis to secure his family’s future as rival forces battle for control of the planet’s precious spice.",
        image: "../Images/Movies/Dune.jpg"
     },
@@ -89,7 +89,7 @@ const movieInfo = {
         genre: 'Action, Adventure, Science Fiction',
         duration: '166 min',
         release: '2024',
-        actors: 'Timothée Chalamet Zendaya Rebecca Ferguson Josh Brolin',
+        actors: 'Timothée Chalamet, Zendaya, Rebecca Ferguson, Josh Brolin',
         overview: "Paul Atreides unites with the Fremen to wage war against the conspirators who destroyed his family and to decide the fate of Arrakis.",
         image: "../Images/Movies/Dune_Part_Two_poster.jpeg"
     },
@@ -99,7 +99,7 @@ const movieInfo = {
         genre: 'Horror, Science Fiction',
         duration: '117 min',
         release: '1979',
-        actors: 'Sigourney Weaver Tom Skerritt Veronica Cartwright John Hurt',
+        actors: 'Sigourney Weaver, Tom Skerritt, Veronica Cartwright, John Hurt',
         overview: "The crew of a commercial spaceship encounters a deadly lifeform after investigating a distress signal on a remote planet.",
         image: "../Images/Movies/Alien.jpg"
     },
@@ -109,7 +109,7 @@ const movieInfo = {
         genre: 'Drama, History',
         duration: '140 min',
         release: '1995',
-        actors: 'Tom Hanks Bill Paxton Kevin Bacon Gary Sinise Ed Harris',
+        actors: 'Tom Hanks, Bill Paxton, Kevin Bacon, Gary Sinise, Ed Harris',
         overview: "NASA must devise a strategy to return Apollo 13 to Earth safely after the spacecraft undergoes massive internal damage.",
         image: "../Images/Movies/Apollo13.jpg"
     },
@@ -119,7 +119,7 @@ const movieInfo = {
         genre: 'Action, Adventure, Fantasy',
         duration: '121 min',
         release: '1977',
-        actors: 'Mark Hamill Harrison Ford Carrie Fisher Alec Guinness',
+        actors: 'Mark Hamill, Harrison Ford, Carrie Fisher, Alec Guinness',
         overview: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy.",
         image: "../Images/Movies/StarWars.jpg"
     },
@@ -129,7 +129,7 @@ const movieInfo = {
         genre: 'Drama, Science Fiction',
         duration: '116 min',
         release: '2016',
-        actors: 'Amy Adams Jeremy Renner Forest Whitaker',
+        actors: 'Amy Adams, Jeremy Renner, Forest Whitaker',
         overview: "A linguist works with the military to communicate with alien lifeforms after twelve mysterious spacecraft appear around the world.",
         image: "../Images/Movies/Arrival.jpg"
     },
@@ -139,7 +139,7 @@ const movieInfo = {
         genre: 'Adventure, Drama, Mystery',
         duration: '123 min',
         release: '2019',
-        actors: 'Brad Pitt Tommy Lee Jones Ruth Negga',
+        actors: 'Brad Pitt, Tommy Lee Jones, Ruth Negga',
         overview: "Astronaut Roy McBride undertakes a mission across an unforgiving solar system to uncover the truth about his missing father.",
         image: "../Images/Movies/Astra.jpg"
     },
@@ -149,7 +149,7 @@ const movieInfo = {
         genre: 'Biography, Drama, History',
         duration: '141 min',
         release: '2018',
-        actors: 'Ryan Gosling Claire Foy Jason Clarke',
+        actors: 'Ryan Gosling, Claire Foy, Jason Clarke',
         overview: "A look at the life of the astronaut, Neil Armstrong, and the legendary space mission that led him to become the first man to walk on the Moon.",
         image: "../Images/Movies/FirstMan.png"
     },
@@ -159,7 +159,7 @@ const movieInfo = {
         genre: 'Biography, Drama, History',
         duration: '127 min',
         release: '2016',
-        actors: 'Taraji P. Henson Octavia Spencer Janelle Monáe',
+        actors: 'Taraji P. Henson, Octavia Spencer, Janelle Monáe',
         overview: "The story of a team of female African-American mathematicians who served a vital role in NASA during the early years of the U.S. space program.",
         image: "../Images/Movies/Hidden.jpg"
     },
@@ -169,7 +169,7 @@ const movieInfo = {
         genre: 'Action, Adventure, Science Fiction',
         duration: '162 min',
         release: '2009',
-        actors: 'Sam Worthington Zoe Saldana Sigourney Weaver Stephen Lang',
+        actors: 'Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang',
         overview: "A paralyzed marine is sent to the alien world Pandora and must choose between following orders and protecting his new home.",
         image: "../Images/Movies/Avatar.jpg"
     },
@@ -179,7 +179,7 @@ const movieInfo = {
         genre: 'Action, Adventure, Comedy, Science Fiction',
         duration: '121 min',
         release: '2014',
-        actors: 'Chris Pratt Zoe Saldana Dave Bautista Vin Diesel Bradley Cooper',
+        actors: 'Chris Pratt, Zoe Saldana, Dave Bautista, Vin Diesel, Bradley Cooper',
         overview: "A band of misfit criminals forms an unlikely team to stop a powerful villain from using an artifact to destroy the galaxy.",
         image: "../Images/Movies/Guardians_of_the_Galaxy.jpg"
     },
@@ -209,6 +209,7 @@ for (const movie in movieInfo) {
     newDiv.onclick = function() { clickedMovie(movie) }
     let newImg = document.createElement("img")
     newImg.src = movieInfo[movie].image
+    newImg.alt = movieInfo[movie].title
     newDiv.appendChild(newImg)
     containerDiv.appendChild(newDiv)
 }
@@ -238,14 +239,15 @@ function clickedMovie(movie) {
             })
         }
 
-        title.innerHTML = movieInfo[movie].title
-        overview.innerHTML = movieInfo[movie].overview
-        imdb.innerHTML = movieInfo[movie].imdb
-        genre.innerHTML = movieInfo[movie].genre
-        duration.innerHTML = movieInfo[movie].duration
-        release.innerHTML = movieInfo[movie].release
-        actors.innerHTML = movieInfo[movie].actors
+        title.textContent = movieInfo[movie].title
+        overview.textContent = movieInfo[movie].overview
+        imdb.textContent = movieInfo[movie].imdb
+        genre.textContent = movieInfo[movie].genre
+        duration.textContent = movieInfo[movie].duration
+        release.textContent = movieInfo[movie].release
+        actors.textContent = movieInfo[movie].actors
         movieImage.src = movieInfo[movie].image
+        movieImage.alt = movieInfo[movie].title
     } else {
         document.body.style.cursor = ""
         return
